77b90a87ca07d166f33865c2b4c594bb309cf3ca
ferhatu <ferhat_2001@yahoo.com>
2020-06-17 11:20:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
       });
   });
   return (
-    <select>
+    <select id="select-bar">
       {modes.map((modes, index) => {
         return <option key={index}>{modes.modeName}</option>;
       })}
@@ -21,3 +21,62 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState, useEffect } from 'react';
+// import './App.css';
+// function App() {
+//   const [modes, setModes] = useState([]);
+//   const [selectedMode, setSelectedMode] = useState('');
+//   const [lines, setLines] = useState([])
+//   useEffect(() => {
+//     fetch('https://api.tfl.gov.uk/Line/Meta/Modes')
+//       .then(results => results.json())
+//       .then(data => setModes(data))
+//   }, []);
+//   useEffect(() => {
+//     fetch(`https://api.tfl.gov.uk/Line/Mode/${selectedMode}`)
+//       .then(results => results.json())
+//       .then(data => {
+//         setLines(data)
+//         console.log(data)
+//       })
+//   }, [selectedMode]);
+//   const handleSelectChange = (event) => {
+//     if (event.target.id === 'modes') {
+//       setSelectedMode(event.target.value)
+//     }
+//   }
+
+//   const renderSelect = (defaultOption, options, id) => {
+//     return (
+//       <select onChange={handleSelectChange}
+//         id={id}>
+//         <option selected={true}
+//           value={defaultOption}>
+//           {defaultOption}
+//         </option>
+//         {options.map((option, index) => {
+//           return (
+//             id === 'modes' ? <option key={index + option.modeName}
+//               value={option.modeName}
+//             >
+//               {option.modeName}
+//             </option> :
+//               <option key={index + option.modeName}
+//                 value={option.name}
+//               >
+//                 {option.name}
+//               </option>
+//           )
+//         })}
+//       </select>
+//     )
+//   }
+//   return (
+//     <div className="App">
+//       {renderSelect('Choose a Mode of Transport...', modes, 'modes')}
+//       {lines.length > 0 ? renderSelect('Choose a Line...', lines, 'lines') : null}
+//       <p>You selected line: {selectedMode}</p>
+//     </div>
+//   );
+// }
