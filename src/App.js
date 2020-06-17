1bb59bfd5c9f6e45c4824b24f0646092523b02d1
ferhatu <ferhat_2001@yahoo.com>
2020-06-17 10:46:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [modes, setModes] = useState([]);
+  useEffect(() => {
+    fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
+      .then((res) => res.json())
+      .then((data) => {
+        setModes(data);
+      });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <select>
+      {modes.map((modes, index) => {
+        return <option key={index}>{modes.modeName}</option>;
+      })}
+    </select>
   );
 }
 
